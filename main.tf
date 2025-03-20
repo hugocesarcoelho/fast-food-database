1512aceaeb1df9a8119489479cececa9378f65d1
hugocesarcoelho <hugo_cesar45@hotmail.com>
2025-03-20 20:59:44 -0300

    Adding files.

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "random_password" "db_password" {
+  length  = 16
+  special = false
+}
+
+resource "aws_secretsmanager_secret" "db_secret" {
+  name = "mysql-rds-password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_secret_version" {
+  secret_id     = aws_secretsmanager_secret.db_secret.id
+  secret_string = random_password.db_password.result
+}
+
+resource "random_pet" "sg" {}
+
+resource "aws_security_group" "rds-sg" {
+  name = "${random_pet.sg.id}-rds-sg"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  identifier             = "fiap-mysql-db"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = "admin"
+  password               = random_password.db_password.result
+  db_name                = "fiapdb"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.rds-sg.id]
+
+  tags = {
+    Name = "fiap-mysql-db"
+  }
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
+}
